83236f18d1cb53e26216343c118199060f79f9fe
Kanishkarweb <84016004+Kanishkarweb@users.noreply.github.com>
2024-01-15 19:18:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+tiffen = ["Idly", "Dosa", "Pongal"]
+
+spicyfood = ["Chicken rice ", "Biriyani", "Egg Rice"]
+
+print(tiffen)
+
+
+print(spicyfood)
+
+tiffen.append(spicyfood)
+print(tiffen)
+
+spicyfood.extend(tiffen)
+print(spicyfood)
